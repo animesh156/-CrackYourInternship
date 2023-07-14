24e3d560cf4b14edaecdf145e7e3405676f59226
Animesh  Singh <animeshrathore255@gmail.com>
2023-07-14 23:38:19 +0530

    Create ContainerWater.cpp

---
+++ ContainerWater.cpp
@@ -0,0 +1,16 @@
+
+ int maxArea(vector<int>& v) {
+         int i = 0;
+        int n = v.size();
+        int j = n - 1;
+        int area = 0;
+
+        while(j > i){
+            area = max(area, min(v[i],v[j]) * abs(i - j));
+            if(v[i] < v[j]) i++;
+            else j--;
+        }
+
+        return area;
+        
+    }

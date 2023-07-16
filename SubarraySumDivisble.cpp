e3444719861afcb50cdea20e59431c6b4e711c05
Animesh  Singh <animeshrathore255@gmail.com>
2023-07-16 16:46:43 +0530

    Create SubarraySumDivisble.cpp

---
+++ SubarraySumDivisble.cpp
@@ -0,0 +1,13 @@
+
+int subarraysDivByK(vector<int>& nums, int k) {
+         int n = nums.size();
+        vector<int> pre(k);
+        pre[0]=1;
+        int ans=0,sum=0;
+        for(int x : nums){
+             sum =(sum + x%k +k) %k;
+             ans += pre[sum];
+             pre[sum]++;
+        }
+        return ans;
+    }

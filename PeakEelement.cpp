275564beac719a5a77f17dee780199ca43b3f4ab
Animesh  Singh <animeshrathore255@gmail.com>
2023-07-31 21:07:34 +0530

    Create PeakEelement.cpp

---
+++ PeakEelement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+       sort(nums.begin(),nums.end());
+
+       set<int> s;
+
+       for(auto it:nums){
+    s.insert(it);
+       }
+
+       return
+        
+    }
+};
